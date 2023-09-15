fbfcc777fea31e8708af6243904886c028c16673
NithuRaveendran <nithuraveendran2016@gmail.com>
2023-09-15 17:40:15 +0530

    secondcommit

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ Additems.style.fontWeight = 'bold';
 Additems.style.color = 'green';
 
 
+//GETELEMENTBYCLASSNAME//
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+console.log(items[2]);
+console.log(items[2]);
+
+
+items[3].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight ='bold';
+}
+
+//GETRLEMENTBYTAGNAME//
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(items[5]);
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor ='yellow';
+}
