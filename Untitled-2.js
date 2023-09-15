fda4ff942638c6ec4346ce3045631fc4e36208ca
NithuRaveendran <nithuraveendran2016@gmail.com>
2023-09-15 16:21:51 +0530

    third commit

--- Untitled-2.js
+++ Untitled-2.js
@@ -4,17 +4,15 @@ var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 console.log(items[2]);
+console.log(items[2]);
 
 
-items[1].textContent='Hai';
-items[2].style.fontWeight = 'bold';
-items[1].style.backgroundColor='grey';
+items[3].style.backgroundColor='green';
 
 for(var i=0;i<items.length;i++)
 {
-    items[i].style.backgroundColor ='grey';
+    items[i].style.fontWeight ='bold';
 }
-items[2].style.fontColor='green';
 
 
 
