6aa270ba2807811a1feca6d0164b247317b77173
NithuRaveendran <nithuraveendran2016@gmail.com>
2023-09-17 02:31:55 +0530

    filtercommit

--- index2 code/form.js
+++ index2 code/form.js
@@ -1,11 +1,14 @@
+
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
@@ -47,19 +50,25 @@ function removeItem(e){
   }
 }
 
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
 // edit button
 var editBtn = document.createElement('button');
 
 // Add classes to edit button
 editBtn.className = 'btn btn-danger btn-sm float-right edit';
-// Add classes to edit button
-editBtn.className = 'btn btn-danger btn-sm float-right edit';
-
-// Append text node
-editBtn.appendChild(document.createTextNode('X'));
-
-// Append button to li
-li.appendChild(editBtn);
-
-// Append li to list
-itemList.appendChild(li);
