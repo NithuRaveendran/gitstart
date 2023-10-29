79e3b9a9ee039fcb9ec00ab9e73ffc132bd61754
NithuRaveendran <nithuraveendran2016@gmail.com>
2023-10-29 19:58:32 +0530

    node.js

---
+++ node.js/nodepractice.js
@@ -0,0 +1,30 @@
+//const name = "ram";
+//let age = 24;
+//const hasHobbies = true;
+
+//age=30;
+
+
+//function summarizeUser(userName, userAge, userHasHobby) {
+    //return 'Name is ' + userName + ', Age is ' + userAge + ', and the user has hobbies ' + userHasHobby;
+//}
+  
+ //console.log(summarizeUser(name, age, hasHobbies));
+
+//----ARROW FUNCTION------//
+
+let  name = "ram";
+let age = 24;
+const hasHobbies = true;
+
+
+
+
+const summarizeUser=(userName, userAge, userHasHobby)=> {
+    return 'Name is ' + userName + ', Age is ' + userAge + ', and the user has hobbies ' + userHasHobby;
+}
+  
+ console.log(summarizeUser(name, age, hasHobbies));
+
+
+
---
+++ node.js/objectcreation.js
@@ -0,0 +1,9 @@
+const student={
+    name:"jhon",
+    Age:20,
+    roll_no:32,
+    greet() {
+        console.log('Hi,I am ' + this.name);
+    }
+};
+student.greet();
